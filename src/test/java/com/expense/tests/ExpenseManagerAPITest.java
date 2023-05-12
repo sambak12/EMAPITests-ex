44b344799a4d136b1755696fb2e6a275aaf21cca
sambak12 <120355568+sambak12@users.noreply.github.com>
2023-05-12 16:06:18 +0530

    Update ExpenseManagerAPITest.java

--- src/test/java/com/expense/tests/ExpenseManagerAPITest.java
+++ src/test/java/com/expense/tests/ExpenseManagerAPITest.java
@@ -9,18 +9,22 @@ import io.restassured.response.Response;
 
 public class ExpenseManagerAPITest {
 
+	public RestAssured.port = 8089;
 	@Test
 	public void test1() {
-		
-		RestAssured.port = 8089;
 		Response res = RestAssured.get("http://localhost:8089/ExpenseApp-1/login.jsp");
-		
 		int code = res.getStatusCode();
-		
 		assertEquals(code, 200);
-		
-		System.out.println(code);
-		
+		System.out.println(code);	
 	}
+	
+	@Test
+	public void Test2(){
+		Response res = RestAssured.get("http://localhost:8089/ExpenseApp-1/login.jsp");
+		int time = res.getTime();
+		assertEquals(time, 5);
+		System.out.println(time);
+	}
+		
 
 }
